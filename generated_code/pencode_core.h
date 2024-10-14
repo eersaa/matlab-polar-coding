14be3937921992c80ac9acc757b206899a95ebdd
Eerik Saarinen <eerik.saarinen@oulu.fi>
2024-10-14 16:55:53 +0300

    Generated code with double datatypes
    All datatypes are double type.

---
+++ generated_code/pencode_core.h
@@ -0,0 +1,27 @@
+//
+// Academic License - for use in teaching, academic research, and meeting
+// course requirements at degree granting institutions only.  Not for
+// government, commercial, or other organizational use.
+// File: pencode_core.h
+//
+// MATLAB Coder version            : 24.1
+// C/C++ source code generated on  : 14-Oct-2024 16:52:15
+//
+
+#ifndef PENCODE_CORE_H
+#define PENCODE_CORE_H
+
+// Include Files
+#include "rtwtypes.h"
+#include <cstddef>
+#include <cstdlib>
+
+// Function Declarations
+extern void pencode_core(const double d[128], double n, double x[128]);
+
+#endif
+//
+// File trailer for pencode_core.h
+//
+// [EOF]
+//
